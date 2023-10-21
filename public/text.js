c4b57050ff083b763f63135cb696df06673f6e30
Manuel Bulletti <83927784+Manuel-Bulletti@users.noreply.github.com>
2023-10-22 01:46:55 +0200

    Major Changes

--- public/text.js
+++ public/text.js
@@ -1,11 +1,11 @@
-let words = ['This app is an enterprise experiment created to gauge interest in "Cicero".', 'Thank you for your interest!'];
+let words = ['Sorry!', 'This is a statistical analysis made by students from the university of Pisa.', 'Cicero will be available soon!'];
 let part;
 let i = 0;
 let offset = 0;
 let len = words.length;
 let skip_count = 0;
-let skip_delay = 15;
-let speed = 70;
+let skip_delay = 30;
+let speed = 45;
 
 let wordflick = function () {
     let instance = setInterval(() => {
